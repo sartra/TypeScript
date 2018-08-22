f1eee8eb79e3de234b87b93b47b03831c88fd8ef
Sarah Renshaw <srenshaw@nycl137457m.us.dci.discovery.com>
2018-08-22 12:20:48 -0400

    tuple types

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+type TestScores = [string, ...number[]];
+
+const thaliaTestScore = ["Thalia", ...[100, 98, 99, 100]];
+const davidTestScore = ["David", ...[100, 98, 100]];
+
+console.log(thaliaTestScore);
+console.log(davidTestScore);
